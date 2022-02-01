346f378a2f427c7e028f728ae25a0c7352054666
Geraldo <geraldo.vedrossi.neto@gmail>
2022-02-01 12:32:40 -0300

    Subindo exercicios 01/02

---
+++ Eclipse/Exercicios01_02/Animal.java
@@ -0,0 +1,68 @@
+/*
+1- Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e
+comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as
+características de forma que tudo o que for comum a todos os animais fique na classe
+Animal:
+							Animal
+			Cachorro		Cavalo			Preguiça
+			
+			Nome			Nome			Nome
+			Idade			Idade			Idade
+			Emite som		Emite som		Emite som
+			Corre			Corre			Sobe árvores
+ */
+
+package familia44_5;
+
+	public class Animal { //1-Criando Classe
+		
+		private String nomeAnimal; //2-Criando Atributos
+		private int idadeAnimal;
+		private boolean emiteSom;
+		private String deslocamento;
+		
+	//3-Criando metodo construtor da SuperClasse	
+	public Animal(String nomeAnimal, int idadeAnimal, boolean emiteSom, String deslocamento) {
+		super();
+		this.nomeAnimal = nomeAnimal;
+		this.idadeAnimal = idadeAnimal;
+		this.emiteSom = emiteSom;
+		this.deslocamento = deslocamento;
+	}
+
+	//4-Criando Getters & Setters
+	public String getNomeAnimal() {
+		return nomeAnimal;
+	}
+
+	public void setNomeAnimal(String nomeAnimal) {
+		this.nomeAnimal = nomeAnimal;
+	}
+
+	public int getIdadeAnimal() {
+		return idadeAnimal;
+	}
+
+	public void setIdadeAnimal(int idadeAnimal) {
+		this.idadeAnimal = idadeAnimal;
+	}
+
+	public boolean isEmiteSom() {
+		return emiteSom;
+	}
+
+	public void setEmiteSom(boolean emiteSom) {
+		this.emiteSom = emiteSom;
+	}
+
+	public String getDeslocamento() {
+		return deslocamento;
+	}
+
+	public void setDeslocamento(String deslocamento) {
+		this.deslocamento = deslocamento;
+	}
+	
+	
+
+}
